f93492242979ef18abc66a9868d59eeae2c0adb9
navdeepsreddy <82318864+navdeepsreddy@users.noreply.github.com>
2022-08-21 10:36:31 +0530

    Update chat.js

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -60,7 +60,7 @@ function getResponse() {
     let userText = $("#textInput").val();
 
     if (userText == "") {
-        userText = "I love Code Palace!";
+        userText = "I am hacker and coder with navdeep";
     }
 
     let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
